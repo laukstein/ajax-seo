a2052ea24465c4cabf66569a33775cd55d2054ee
Binyamin <7raivis@inbox.lv>
2011-01-19 00:27:26 +0200

    Implement for Ajax JSONP timeout. Better HTML5 formatting.

--- index.php
+++ index.php
@@ -12,13 +12,13 @@ while($row=@mysql_fetch_array($result,MYSQL_ASSOC)){
 ?>
 <html>
 <head>
-<meta charset="utf-8">
+<meta charset=utf-8>
 <title><?=$title;?></title>
-<link type="text/css" href="<?=$path;?>/styles.css" rel="stylesheet">
+<link rel=stylesheet href="<?=$path;?>/styles.css">
 <script src="<?=$path;?>/jquery-1.4.4.min.js"></script>
 <script src="<?=$path;?>/jquery.address.js?crawlable=true&state=<?=$path;?>"></script>
 <script>
-var el=['header','nav','article'];for(var i=el.length-1;i>=0;i--){document.createElement(el[i]);}
+var el=['header','nav','article'];for(var i=el.length-1;i>=0;i--){document.createElement(el[i]);} // Add HTML5 tag support for old browsers
 $.address.init(function(){
     $('li a').address();
 }).change(function(event){
@@ -26,25 +26,31 @@ $.address.init(function(){
         if($(this).attr('href')==($.address.state()+event.path)){
             $(this).parent('li').addClass('selected').focus();
         }else{
-            $(this).parent('li').removeClass('selected');
+            $(this).parent('li').removeClass();
         }
     });
-    $('#content').ajaxStart(function(){$(this).html('Loading...');}); //solution for `beforeSend`
+    $('#content').ajaxStart(function(){     // Solution for beforeSend
+        $(this).html('Loading...');
+    });
+    var timer=window.setTimeout(function(){ // Implement for timeout
+        $('#content').html('Loading seems to be taking a while.');
+    },1000);
     $.ajax({
         type:"GET",
-        url:/*'http://sub.domain.com/'+*/encodeURIComponent(event.path.substr(1))+'.json',
+        url:/*'http://lab.laukstein.com/ajax-seo/'+*/encodeURIComponent(event.path.substr(1))+'.json',
         dataType:'jsonp',
         //jsonp:'callback',
         //cache:false,
         //async:false,
         jsonpCallback:'a',
-        //timeout:5000,
         success:function(data){
+            window.clearTimeout(timer);
             document.title=data.title;
             $('#content').html(data.content);
         },
-        error:function(request,status,error){
-            $('#content').html('The request failed. Try to refresh page. '+status);
+        error:function(){
+            window.clearTimeout(timer);
+            $('#content').html('The request failed.');
         }
     });
 });
@@ -53,19 +59,21 @@ $.address.init(function(){
 <body>
 <header>
 <h1>Ajax SEO</h1>
-<nav><ul>
+<nav>
+<ul>
 <?php
 $result=mysql_query("SELECT * FROM $dbtable ORDER BY orderid ASC");
 while($row=@mysql_fetch_array($result,MYSQL_ASSOC)){
     $row[]=array('row'=>array_map('htmlspecialchars',$row));
     $url=$row['url'];
     $title=$row['title'];
-    echo $nav='      <li';if($_GET['url']==$url){echo ' class="selected"';}echo "><a href=\"$path/$url\" title=\"$title\">$title</a></li>\n";
+    echo $nav='      <li';if($_GET['url']==$url){echo ' class=selected';}echo "><a href=\"$path/$url\" title=\"$title\">$title</a></li>\n";
 }
 ?>
-</ul></nav>
-<article id="content"><?php echo $content; echo $error; mysql_close($conn);?></article>
-<p><a href="https://github.com/laukstein/ajax-seo" title="GitHub repository for Ajax SEO">Latest Ajax SEO in GitHub</a> | <a href="https://github.com/laukstein/ajax-seo/issues" title="Report an issue">Report an issue</a></p>
+</ul>
+</nav>
+<article id=content><?php echo $content; echo $error; mysql_close($conn);?></article>
+<p><a href=//github.com/laukstein/ajax-seo title="GitHub repository for Ajax SEO">Latest Ajax SEO in GitHub</a> | <a href=//github.com/laukstein/ajax-seo/issues title="Report an issue">Report an issue</a></p>
 </header>
 </body>
 </html>
