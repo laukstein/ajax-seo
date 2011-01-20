9616b1e2e8a927e533d363345086acbe342c62d2
Binyamin <7raivis@inbox.lv>
2011-01-21 00:04:45 +0200

    Improved formatting.

--- data.php
+++ data.php
@@ -7,11 +7,11 @@ $result=mysql_query("SELECT * FROM $dbtable WHERE url='$url'");
 while($row=@mysql_fetch_array($result,MYSQL_ASSOC)){
     $row[]=array('row'=>array_map('htmlspecialchars',$row));
     $array=array('url'=>$row['url'],'title'=>$row['title'],'content'=>$row['content']);
-    $json=str_replace('\'','\\',json_encode($array));//$url=$row['url'];$title=$row['title'];$content=$row['content'];echo "{\"url\":\"$url\",\"title\":\"$title\",\"content\":\"$content\"}";
+    $json=str_replace('\'','\\',json_encode($array)); // $url=$row['url'];$title=$row['title'];$content=$row['content'];echo "{\"url\":\"$url\",\"title\":\"$title\",\"content\":\"$content\"}";
     if($_GET['callback']){
-    	echo $_GET['callback']."($json)"; // JSONP
+    	echo "{$_GET['callback']}($json)"; // JSONP
     }else{
-    	echo $json; // JSON
+    	echo $json;                        // JSON
     }
 }
 mysql_close($conn);
