f6bcbd9ce98ebd8b2afb9e093052b60327b7b414
Binyamin <7raivis@inbox.lv>
2011-01-27 01:29:24 +0200

    Google CDN for jQuery with fallback. Optimized asynchronous Google Analytics snippet.

--- index.php
+++ index.php
@@ -19,7 +19,8 @@ if(str_replace('\\','/',pathinfo($_SERVER['SCRIPT_NAME'],PATHINFO_DIRNAME))!='/'
 <link rel=stylesheet href=<?=$path;?>styles.css>
 <meta name=description content="Ajax SEO maximized performance - speed, availability, user-friendly">
 <meta name=keywords content=ajax,seo,crawl,perform,speed,availability>
-<script src=<?=$path;?>jquery-1.4.4.min.js></script>
+<script src=//ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.js></script>
+<script>!window.jQuery&&document.write(unescape('%3Cscript src=<?=$path;?>jquery-1.4.4.min.js%3E%3C/script%3E'))</script>
 <script src=<?=$path;?>jquery.address.js?crawlable=true&state=<?=substr($path,0,-1);?>></script>
 <script>
 var el=['header','nav','article'];for(var i=el.length-1;i>=0;i--){document.createElement(el[i]);} // Add HTML5 tag support for old browsers
@@ -78,5 +79,6 @@ while($row=@mysql_fetch_array($result,MYSQL_ASSOC)){
 <article id=content><?php echo $content; echo $error; mysql_close($con);?></article>
 <p><a href=//github.com/laukstein/ajax-seo title="GitHub repository for Ajax SEO">Latest Ajax SEO in GitHub</a> | <a href=//github.com/laukstein/ajax-seo/zipball/master title="Download latest Ajax SEO from GitHub">Download</a> | <a href=//github.com/laukstein/ajax-seo/issues title="Report an issue">Report an issue</a></p>
 </header>
+<script>var _gaq=[['_setAccount','UA-11883501-1'],['_setDomainName','.laukstein.com'],['_trackPageview']];(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;g.src='//www.google-analytics.com/ga.js';s.parentNode.insertBefore(g,s)}(document,'script'))</script>
 </body>
 </html>
