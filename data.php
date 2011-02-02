5c4e87fa1b0934e894096e297e3a4db596afbbde
Binyamin <7raivis@inbox.lv>
2011-02-02 23:50:05 +0200

    PHP improvements. Added heading h1 in JSON content.

--- data.php
+++ data.php
@@ -6,13 +6,9 @@ header('Access-Control-Allow-Origin: *');
 $result=mysql_query("SELECT url,title,content FROM $dbtable WHERE url='$url'");
 while($row=@mysql_fetch_array($result,MYSQL_ASSOC)){
     $row[]=array('row'=>array_map('htmlspecialchars',$row));
-    $array=array('url'=>$row['url'],'title'=>$row['title'],'content'=>$row['content']);
-    $json=str_replace('\'','\\',json_encode($array)); // $url=$row['url'];$title=$row['title'];$content=$row['content'];echo "{\"url\":\"$url\",\"title\":\"$title\",\"content\":\"$content\"}";
-    if($_GET['callback']){
-    	echo "{$_GET['callback']}($json)"; // JSONP
-    }else{
-    	echo $json;                        // JSON
-    }
+    $array=array('url'=>$row['url'],'title'=>$row['title'],'content'=>"<h1>{$row['title']}</h1>\n{$row['content']}\n");
+    $json=str_replace('\'','\\',json_encode($array));
+    echo (isset($_GET['callback']) ? mysql_real_escape_string($_GET['callback']).'('.$json.')' : $json);
 }
 mysql_close($con);
 ?>
