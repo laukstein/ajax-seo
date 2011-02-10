2aab959c4eb1d8047dc08db036a9452736717422
Binyamin <7raivis@inbox.lv>
2011-02-10 23:37:20 +0200

    str_replace issue and added html paragraph for SEO improvment.

--- data.php
+++ data.php
@@ -6,8 +6,8 @@ header('Access-Control-Allow-Origin: *');
 $result=mysql_query("SELECT url,title,content FROM $dbtable WHERE url='$url'");
 while($row=@mysql_fetch_array($result,MYSQL_ASSOC)){
     $row[]=array('row'=>array_map('htmlspecialchars',$row));
-    $array=array('url'=>$row['url'],'title'=>$row['title'],'content'=>"<h1>{$row['title']}</h1>\n{$row['content']}\n");
-    $json=str_replace('\'','\\',json_encode($array));
+    $array=array('url'=>$row['url'],'title'=>$row['title'],'content'=>"<h1>{$row['title']}</h1>\r\n<p>{$row['content']}</p>\r\n");
+    $json=str_replace('\\/','/',json_encode($array));
     echo (isset($_GET['callback']) ? mysql_real_escape_string($_GET['callback']).'('.$json.')' : $json);
 }
 mysql_close($con);
