e17ca45ae6bbda3a18769eab5aac6bf5b834e054
Binyamin <7raivis@inbox.lv>
2011-03-08 01:18:15 +0200

    Better syntax.

--- content/cache.php
+++ content/cache.php
@@ -3,7 +3,8 @@ function cache($file,$timestamp){
     $gmtime=gmdate('D, d M Y H:i:s T',$timestamp);
     if(isset($_SERVER['HTTP_IF_MODIFIED_SINCE'])||isset($_SERVER['HTTP_IF_NONE_MATCH'])){
         if($_SERVER['HTTP_IF_MODIFIED_SINCE']==$gmtime||str_replace('"','',stripslashes($_SERVER['HTTP_IF_NONE_MATCH']))==md5($timestamp.$file)){
-            header('Status:304 Not Modified',true,304);exit();
+            header('Status:304 Not Modified',true,304);
+            exit();
         }
     }
     header("Last-Modified:$gmtime");
