f49c2faec65634f08efb620243bec820a8fe7ed8
Binyamin <7raivis@inbox.lv>
2012-03-16 03:18:26 +0200

    Set the MySQL global server time zone. Improved X-Robots-Tag HTTP header. Applied PHP formatter.

--- content/connect.php
+++ content/connect.php
@@ -1,60 +1,69 @@
 <?php
 // MySQL settings
-define('MYSQL_DB','test');
-define('MYSQL_USER','root');
-define('MYSQL_PASS','');
-define('MYSQL_HOST','localhost');
-define('MYSQL_TABLE','ajax_seo');
-define('MYSQL_CON',true);
-define('MYSQL_ERROR',false);
+define('MYSQL_DB', 'test');
+define('MYSQL_USER', 'root');
+define('MYSQL_PASS', '');
+define('MYSQL_HOST', 'localhost');
+define('MYSQL_TABLE', 'ajax_seo');
+define('MYSQL_CON', true);
+define('MYSQL_ERROR', false);
 
-$con=@mysql_connect(MYSQL_HOST,MYSQL_USER,MYSQL_PASS);
-$f='content/connect.php';
+$con = @mysql_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASS);
+$f   = 'content/connect.php';
 
-# Return dir path
-if(str_replace('\\','/',pathinfo($_SERVER['SCRIPT_NAME'],PATHINFO_DIRNAME))!='/'){
-    $path=str_replace('\\','/',pathinfo($_SERVER['SCRIPT_NAME'],PATHINFO_DIRNAME)).'/';
-}else{
-    $path=str_replace('\\','/',pathinfo($_SERVER['SCRIPT_NAME'],PATHINFO_DIRNAME));
+//date_default_timezone_set('Etc/GMT');
+
+// Return dir path
+if (str_replace('\\', '/', pathinfo($_SERVER['SCRIPT_NAME'], PATHINFO_DIRNAME)) != '/') {
+    $path = str_replace('\\', '/', pathinfo($_SERVER['SCRIPT_NAME'], PATHINFO_DIRNAME)) . '/';
+} else {
+    $path = str_replace('\\', '/', pathinfo($_SERVER['SCRIPT_NAME'], PATHINFO_DIRNAME));
 }
 
-if(@mysql_select_db(MYSQL_DB,$con)){
+if (@mysql_select_db(MYSQL_DB, $con)) {
     // Define MySQL connection status
-    if(!MYSQL_CON){
-        $change=file_get_contents($f);
-        $change=preg_replace("/define\('(MYSQL_CON)',false\);/","define('$1',true);",$change);
-        $change=preg_replace("/define\('(MYSQL_ERROR)',true\);/","define('$1',false);",$change);
-        if(!@is_writable($f)){chmod($f,0755);} // Change connect.php file permissions if needed
-        $fopen=fopen($f,'w');
-        fwrite($fopen,$change);
+    if (!MYSQL_CON) {
+        $change = file_get_contents($f);
+        $change = preg_replace("/define\('(MYSQL_CON)', false\);/", "define('$1', true);", $change);
+        $change = preg_replace("/define\('(MYSQL_ERROR)', true\);/", "define('$1', false);", $change);
+        if (!@is_writable($f)) {
+            chmod($f, 0755);
+        } // Change connect.php file permissions if needed
+        $fopen = fopen($f, 'w');
+        fwrite($fopen, $change);
         fclose($fopen);
-        header('Location:'.$_SERVER['REQUEST_URI']);
+        header("Location: {$_SERVER['REQUEST_URI']}");
         exit;
     }
     
-    array_map('trim',$_GET);
-    array_map('stripslashes',$_GET);
-    array_map('mysql_real_escape_string',$_GET);
+    array_map('trim', $_GET);
+    array_map('stripslashes', $_GET);
+    array_map('mysql_real_escape_string', $_GET);
     mysql_query("SET NAMES 'utf8'");
     
-    $url=(isset($_GET['url'])? $_GET['url']: null);
-    $urlid=(isset($urlid)? $urlid: null);
-
+    $url   = isset($_GET['url']) ? $_GET['url'] : null;
+    $urlid = isset($urlid) ? $urlid : null;
+    
     // Return 404 error, if url does not exist
-    class validate{
+    class validate
+    {
         public $title;
         public $content;
-        function status(){
-            header('Status:404 Not Found',true,404);
-            $this->title='404 Page not found';
-            $this->content='Sorry, this page cannot be found.';
+        function status()
+        {
+            header('Status: 404 Not Found', true, 404);
+            $this->title   = '404 Not Found';
+            $this->content = 'Sorry, this page cannot be found.';
         }
     }
     
-    $sql="SELECT * FROM ".MYSQL_TABLE;
-    if(!mysql_query($sql)){
+    $sql = "SELECT * FROM " . MYSQL_TABLE;
+    if (!mysql_query($sql)) {
+        // Set the global server time zone, needs for SUPER privileges
+        mysql_query("SET GLOBAL time_zone = '" . date('T') . "'");
+        
         // Create table
-        mysql_query("CREATE TABLE IF NOT EXISTS `".MYSQL_TABLE."` (
+        mysql_query("CREATE TABLE IF NOT EXISTS `" . MYSQL_TABLE . "` (
           `id` mediumint(8) NOT NULL AUTO_INCREMENT,
           `order` mediumint(8) NOT NULL,
           `url` varchar(70) NOT NULL,
@@ -62,26 +71,31 @@ if(@mysql_select_db(MYSQL_DB,$con)){
           `name` text NOT NULL,
           `content` text NOT NULL,
           `pubdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-          PRIMARY KEY  (`id`,`url`),
+          PRIMARY KEY (`id`, `url`),
           UNIQUE KEY `order` (`order`)
         ) ENGINE=MyISAM DEFAULT CHARSET=utf8");
         
         // Insert data
-        $now=date('Y-m-d H:i:s');
-        mysql_query("INSERT INTO `".MYSQL_TABLE."` (`order`,url,name,title,content,pubdate) VALUES
-          (1,'','Home','','Home content','$now'),
-          (2,'about','About','','About content','$now'),
-          (3,'portfolio','Portfolio','Portfolio','Portfolio content','$now'),
-          (4,'contact','Contact','Contact us','Contact content','$now'),
-          (5,'контакты','Контакты','','Содержание контактом','$now'),
-          (6,'צור-קשר','צור קשר','','תוכן לצור קשר','$now');");
+        $now = date('Y-m-d H:i:s');
+        mysql_query("INSERT INTO `" . MYSQL_TABLE . "` (`order`, url, name, title, content, pubdate) VALUES
+          (1, '', 'Home', '', 'Home content', '$now'),
+          (2, 'about', 'About', '', 'About content', '$now'),
+          (3, 'portfolio', 'Portfolio', 'Portfolio', 'Portfolio content', '$now'),
+          (4, 'contact', 'Contact', 'Contact us', 'Contact content', '$now'),
+          (5, 'контакты', 'Контакты', '', 'Содержание контактом', '$now'),
+          (6, 'צור-קשר' ,'צור קשר' ,'' ,'תוכן לצור קשר', '$now');");
         
-        if(is_writable($f)){chmod($f,0600);}
-        header('Content-Type:text/html');
-        header('X-Robots-Tag: index, noarchive, nosnippet',true);
-        $note='Congratulations, installation has completed successfully.';
+        if (is_writable($f)) {
+            chmod($f, 0600);
+        }
+        
+        header('Content-Type: text/html');
+        
+        // Valid indexing & serving directives https://developers.google.com/webmasters/control-crawl-index/docs/robots_meta_tag
+        header('X-Robots-Tag: none', true);
+        $note = 'Congratulations, installation has completed successfully.';
     }
-}else{
+} else {
     // Installer on not reachable database
     include('content/install.php');
 }
