ed7eb84d806b4bc0d04b98b140c89abe26903b5a
Binyamin <7raivis@inbox.lv>
2012-03-16 03:21:38 +0200

    Improved function date() and function cache() usibility, avoided functions runing in case there are no use for that. Applied PHP formatter.

--- content/cache.php
+++ content/cache.php
@@ -1,30 +1,40 @@
 <?php
-class datemod{
+class datemod
+{
     public $gmtime;
-    function date($files,$dbtable,$url){
-        foreach($files as $val){
-            $mod=date('Y/m/d H:i:s',filemtime($val));
-            $array[]=$mod;
-        }
-        $fmod=max($array);
-        $result=mysql_query("SELECT DATE_FORMAT(pubdate,'%Y/%m/%d %H:%i:%s') AS pubdate FROM ".MYSQL_TABLE." WHERE url='$url'");
-        while($row=@mysql_fetch_array($result,MYSQL_ASSOC)){
-            $row[]=array('row'=>array_map('htmlspecialchars',$row));
-            $pubdate=$row['pubdate'];
-        }
-        $pubdate=(isset($pubdate)? $pubdate: null);
-        $maxmod=max($fmod,$pubdate);
-        date_default_timezone_set('Etc/GMT');
-        $this->gmtime=date('D, d M Y H:i:s T',strtotime($maxmod));
-    }
-    function cache($gmtime){
-        if(isset($_SERVER['HTTP_IF_MODIFIED_SINCE'])||isset($_SERVER['HTTP_IF_NONE_MATCH'])){
-            if($_SERVER['HTTP_IF_MODIFIED_SINCE']==$gmtime){
-                header('Status:304 Not Modified',true,304);
-                exit;
+    
+    function date($files, $dbtable, $url)
+    {
+        $result = mysql_query("SELECT DATE_FORMAT(pubdate, '%a, %d %b %Y %T') AS pubdate FROM " . MYSQL_TABLE . " WHERE url = '$url'");
+        if (mysql_num_rows($result)) {
+            foreach ($files as $val) {
+                $mod     = date('D, d M Y H:i:s T', filemtime($val));
+                $array[] = $mod;
             }
+            
+            $fmod = max($array);
+            
+            while ($row = @mysql_fetch_array($result, MYSQL_ASSOC)) {
+                $row[]   = array(
+                    'row' => array_map('htmlspecialchars', $row)
+                );
+                $pubdate = $row['pubdate'] . date(' T');
+            }
+            $this->gmtime = date('Y-m-d H:i:s', strtotime($fmod)) >= date('Y-m-d H:i:s', strtotime($pubdate)) ? $fmod : $pubdate;
         }
-        header("Last-Modified:$gmtime");
+    }
+    
+    function cache($gmtime)
+    {
+        if (isset($gmtime)) {
+            if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) || isset($_SERVER['HTTP_IF_NONE_MATCH'])) {
+                if ($_SERVER['HTTP_IF_MODIFIED_SINCE'] == $gmtime) {
+                    header('Status: 304 Not Modified', true, 304);
+                    exit;
+                }
+            }
+            header("Last-Modified: $gmtime");
+        }
     }
 }
 ?>
