7eef534d531b75190c6242685fa7f52268854968
Binyamin <7raivis@inbox.lv>
2013-07-10 22:58:16 +0300

    Updated assets specification and markup

--- content/connect.php
+++ content/connect.php
@@ -2,47 +2,41 @@
 
 // MySQL settings
 // --------------------------------------------------
-define('MYSQL_DB', 'test');
-define('MYSQL_HOST', 'localhost');
-define('MYSQL_USER', 'root');
-define('MYSQL_PASS', '');
-define('MYSQL_TABLE', 'ajax-seo');
-define('MYSQL_CON', true);
-define('MYSQL_ERROR', false);
-
-
+define('database', 'test');
+define('hostname', 'localhost');
+define('username', 'root');
+define('password', '');
+define('table', 'ajax-seo');
+define('connection', true);
+define('error', false);
+define('cdn', null);
 
 // Path for static assets
-// --------------------------------------------------
-define('CDN_PATH', null);
-$issetcdn   = CDN_PATH ? true : false;
-$cdn_host   = parse_url(CDN_PATH, PHP_URL_HOST);
-$cdn_scheme = parse_url(CDN_PATH, PHP_URL_SCHEME);
+$issetcdn   = cdn ? true : false;
+$cdn_host   = parse_url(cdn, PHP_URL_HOST);
+$cdn_scheme = parse_url(cdn, PHP_URL_SCHEME);
 $cdn_scheme = isset($cdn_scheme) ? $cdn_scheme . '://' : '//';
-$assets      = $issetcdn ? CDN_PATH : $rootpath . '/assets/';
+$cdn_uri    = $cdn_scheme . $cdn_host;
+$debug      = isset($debug) ? $debug : null;
+$path       = isset($path) ? $path : null;
+$assets     = $debug ? $path : ($issetcdn ? cdn : $path);
 
-
-
-// Connect to db
-// --------------------------------------------------
-$con = @mysql_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASS);
-
-
-
+$con = @mysql_connect(hostname, username, password); // Connect to db
 $f   = 'content/connect.php';
 
-if (@mysql_select_db(MYSQL_DB, $con)) {
-    // Define MySQL connection status
-    if (!MYSQL_CON) {
+if (@mysql_select_db(database, $con)) {
+    if (!connection) { // Define MySQL connection status
         $change = file_get_contents($f);
-        $change = preg_replace("/define\('(MYSQL_CON)', false\);/", "define('$1', true);", $change);
-        $change = preg_replace("/define\('(MYSQL_ERROR)', true\);/", "define('$1', false);", $change);
+        $change = preg_replace("/define\('(connection)', false\);/", "define('$1', true);", $change);
+        $change = preg_replace("/define\('(error)', true\);/", "define('$1', false);", $change);
 
         // Change connect.php file permissions if needed
         if (!@is_writable($f)) {
             chmod($f, 0755);
         }
+
         $fopen = fopen($f, 'w');
+
         fwrite($fopen, $change);
         fclose($fopen);
 
@@ -57,7 +51,7 @@ if (@mysql_select_db(MYSQL_DB, $con)) {
     $url   = isset($_GET['url']) ? $_GET['url'] : null;
     $urlid = isset($urlid) ? $urlid : null;
 
-    if (!mysql_query('SELECT * FROM `' . MYSQL_TABLE .'`')) {
+    if (!mysql_query('SELECT * FROM `' . table .'`')) {
         // Set the global server timezone to GMT, needs for SUPER privileges
         mysql_query("SET GLOBAL time_zone = '+00:00'");
 
@@ -71,7 +65,7 @@ if (@mysql_select_db(MYSQL_DB, $con)) {
         }
 
         // Create table
-        mysql_query('CREATE TABLE IF NOT EXISTS `' . MYSQL_TABLE . '` (
+        mysql_query('CREATE TABLE IF NOT EXISTS `' . table . '` (
               id int AUTO_INCREMENT PRIMARY KEY,
               array int NOT NULL,
               url char(70) NOT NULL,
@@ -82,14 +76,12 @@ if (@mysql_select_db(MYSQL_DB, $con)) {
               updated datetime NOT NULL,
               created timestamp DEFAULT current_timestamp
             ) ENGINE=MyISAM DEFAULT ' . $char);
-
         // Create trigger (needs TRIGGER global privilege)
         mysql_query('CREATE TRIGGER updated BEFORE
-            UPDATE ON `' . MYSQL_TABLE . '`
+            UPDATE ON `' . table . '`
               FOR EACH ROW SET new.updated=NOW()');
-
         // Insert data
-        mysql_query("INSERT INTO `" . MYSQL_TABLE . "` (array, url, `meta-title`, `meta-description`, title, content) VALUES
+        mysql_query("INSERT INTO `" . table . "` (array, url, `meta-title`, `meta-description`, title, content) VALUES
             (1, '', 'Home', 'Ajax SEO is crawlable framework for Ajax applications.', 'Make Apps crawable', '<h2>Improve your user experience</h2>\n<p>Ajax SEO is crawlable framework for Ajax applications that uses the latest SEO standards, Page Speed and YSlow rules, Google HTML/CSS Style Guide, etc. to improve and maximize performance, security, accessibility, usability and user experience.</p>\n<p>The source code is build on latest W3C standards, HTML Living Standard HTML5, CSS3, Microdata, etc.<br>\nCheck <a class=js-as href=history>history</a> feature and after <a href=javascript:history.forward()>history.forward()</a>.</p>'),
             (2, 'history', 'History', '', 'Manage history', '<p>Try <a href=javascript:history.back()>history.back()</a> and check <a class=js-as href=bind>bind event</a>.</p>'),
             (3, 'bind', 'Bind', '', 'Bind event', '<p>Bind on Ajax loaded <a class=js-as href=test/nested.html>content</a> with class=js-as.</p>'),
