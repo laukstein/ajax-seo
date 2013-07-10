c26a7472a56a231d5ae75b3e079c25ae1c637e6a
Binyamin <7raivis@inbox.lv>
2013-07-10 23:04:29 +0300

    Specification reorder, assets definition
    Specification reorder according to priority
    Assets and absolute path definition
    Simplification

--- content/config.php
+++ content/config.php
@@ -5,18 +5,41 @@
 // --------------------------------------------------
 
 
-
 // Debug mode
 // --------------------------------------------------
-$ip = array('127.0.0.1', 'localhost');
-if (in_array($_SERVER['REMOTE_ADDR'], $ip)) {
+if (in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1'))) {
     error_reporting(E_ALL);
+
     $debug = true;
+
+    // Assets for development
+    $rand = '?' . rand();
+    $css  = 'style.css' . $rand;
+    $js   = 'jquery.address.js' . $rand;
 } else {
     error_reporting(0);
+
     $debug = false;
+
+    // Assets for production
+    $css = 'style.min.css';
+    $js  = 'jquery.address.min.js';
 }
 
+
+// Prevent XSS and SQL Injection
+// --------------------------------------------------
+$host = $_SERVER['SERVER_NAME'];
+
+if (strpos($_SERVER['HTTP_HOST'], $host) === false) {
+    http_response_code(400);
+
+    // Robots meta tag and X-Robots-Tag HTTP header specifications https://developers.google.com/webmasters/control-crawl-index/docs/robots_meta_tag
+    header('X-Robots-Tag: none');
+
+    header('Content-Type: text/plain');
+    exit('400 Bad Request');
+}
 
 
 // Compatibility
@@ -32,12 +55,11 @@ if (function_exists('apache_get_modules')) {
         $fix       .= "\n* enable Apache mod_rewrite";
     }
 }
-
-// PHP 5.2 backward compatibility
 if (version_compare($phpv, '5.4', '<')) {
     // Add latest PHP functions
     include 'content/function.http-response-code.php';
 }
+// PHP 5.2 backward compatibility
 if (version_compare($phpv, '5.2', '>=')) {
     // date.timezone settings required since PHP 5.3
     if (version_compare($phpv, '5.3', '>=') && !ini_get('date.timezone')) {
@@ -51,48 +73,19 @@ if (version_compare($phpv, '5.2', '>=')) {
     $compatible = false;
     $fix       .= "\n* upgrade to PHP 5.2 or later";
 }
-
 if (!$compatible) {
     http_response_code(503);
     header('Retry-After: 3600'); // 1 hour
     header('X-Robots-Tag: none');
     header('Content-Type: text/plain');
-    exit('Your server is outdated https://github.com/laukstein/ajax-seo#how-to-use' . $fix);
+    exit('Your server is outdated' . $fix);
 }
-
 
 
 // Common variables
 // --------------------------------------------------
 $scheme = $_SERVER['SERVER_PORT'] == 443 ? 'https' : 'http';
-$host   = $_SERVER['SERVER_NAME'];
-$path   = rawurldecode($_SERVER['REQUEST_URI']);
-$uri    = $scheme . '://' . $host . $path;
-
-
-
-// Prevent XSS and SQL Injection
-// --------------------------------------------------
-if (strpos($_SERVER['HTTP_HOST'], $_SERVER['SERVER_NAME']) === false) {
-    http_response_code(400);
-
-    // Robots meta tag and X-Robots-Tag HTTP header specifications https://developers.google.com/webmasters/control-crawl-index/docs/robots_meta_tag
-    header('X-Robots-Tag: none');
-
-    header('Content-Type: text/plain');
-    exit('400 Bad Request');
-}
-
-
-
-// Return dir path
-if (str_replace('\\', '/', pathinfo($_SERVER['SCRIPT_NAME'], PATHINFO_DIRNAME)) != '/') {
-    $path = str_replace('\\', '/', pathinfo($_SERVER['SCRIPT_NAME'], PATHINFO_DIRNAME)) . '/';
-} else {
-    $path = str_replace('\\', '/', pathinfo($_SERVER['SCRIPT_NAME'], PATHINFO_DIRNAME));
-}
-
-
-
-// Return root path
-$rootpath = substr($path, 0, -1);
+$uri    = $scheme . '://' . $host . rawurldecode($_SERVER['REQUEST_URI']);
+// Absolute path
+$dir    = str_replace('\\', '/', pathinfo($_SERVER['SCRIPT_NAME'], PATHINFO_DIRNAME));
+$path   = $dir == '/' ? $dir : $dir . '/';
