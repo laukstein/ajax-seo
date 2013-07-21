ac17638b083e1a1bbfa88602f82c694a483761a0
Binyamin <7raivis@inbox.lv>
2013-07-21 19:42:22 +0300

    Markup

--- index.php
+++ index.php
@@ -97,7 +97,7 @@ $metadata .= "\n<meta name=referrer content=never>";
 $metadata .= "\n<meta name=viewport content=\"width=device-width, user-scalable=0\">";
 
 // Authorship in Google Search http://support.google.com/webmasters/bin/answer.py?hl=en&answer=1408986
-//$metadata .= "\n<link rel=author href=https://plus.google.com/000000000000000000000>";
+// $metadata .= "\n<link rel=author href=https://plus.google.com/000000000000000000000>";
 
 // Prefetch CDN by saving DNS resolution time https://github.com/h5bp/html5-boilerplate/blob/master/doc/extend.md#dns-prefetching
 if ($issetcdn) {
