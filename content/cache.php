9af4eb025ee4ddd1e16ade6eaa60de75f93cf810
Binyamin <7raivis@inbox.lv>
2013-12-09 22:08:47 +0200

    HTTP cache related to update date

--- content/cache.php
+++ content/cache.php
@@ -1,76 +1,100 @@
 <?php
-
-$dcon = defined('connection') ? (connection ? true : false) : false;
-
+//
 // Static vs singleton class http://moisadoru.wordpress.com/2010/03/02/static-call-versus-singleton-call-in-php/
-class datemod {
-    //public function __construct($mysqli) {} // MySQLi in class http://www.weblimner.com/tutorial/using-mysqli-in-a-seperate-class/
+//
+
+$dbcon = defined('connection') ? connection : false;
+
+class cache {
+    private static $dbcon;
+
     // HTTP header cache
-    public static function cache() {
-        global $dcon, $mysqli, $url;
+    private static function http($date) {
+        // $date = strtotime($date);
+        // if (!$date) return false;
+        $date = date('D, d M Y H:i:s T', strtotime($date));
 
-        $update = $fmod = date('Y-m-d H:i:s', filemtime($_SERVER['SCRIPT_FILENAME']));
-
-        if ($dcon) {
-            $stmt = isset($url) ? $mysqli->prepare("SELECT GREATEST(updated, created) AS date FROM `" . table . "` WHERE url = ? LIMIT 1")
-                                : $mysqli->prepare("SELECT MAX(GREATEST(updated, created)) AS date FROM `" . table . "`");
-            if (isset($url)) {
-                $stmt->bind_param('s', $url);
-            }
-            $stmt->execute();
-            $stmt->bind_result($date);
-
-            while ($stmt->fetch()) {
-                $update = max($fmod, $date);
-            }
-
-            $stmt->free_result();
-            $stmt->close();
-        }
-
-        $update = date('D, d M Y H:i:s T', strtotime($update));
-
-        if ((isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) || isset($_SERVER['HTTP_IF_NONE_MATCH'])) && $_SERVER['HTTP_IF_MODIFIED_SINCE'] == $update) {
+        if ((isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) || isset($_SERVER['HTTP_IF_NONE_MATCH'])) && $_SERVER['HTTP_IF_MODIFIED_SINCE'] == $date) {
             http_response_code(304);
             ob_end_clean(); // Empty the response body
             exit;
         }
 
-        header('Last-Modified: '. $update);
+        header('Last-Modified: '. $date);
     }
 
-    // Recent file update
-    public static function date() {
-        global $dcon, $mysqli, $url;
+    // Identified URL cache
+    public static function url() {
+        self::http(self::$dbcon ? max(date::url(), date::file('php')) : date::file('php'));
+    }
 
-        $directory = new RecursiveDirectoryIterator('.');
-        $iterator  = new RecursiveIteratorIterator($directory);
-        $regex     = new RegexIterator($iterator, '/.+\.(appcache|css|js|php|txt|xml)/');
-        $modfile   = array();
+    // The opened file
+    public static function me() {
+        self::http(date::me());
+    }
 
-        foreach($regex as $key) {
-            $modfile[] = $key->getMTime();
-        }
+    // The opened file vs latest database update
+    public static function medb() {
+        self::http(self::$dbcon ? max(date::db(), date::me()) : date::me());
+    }
 
-        $update = date('Y-m-d H:i:s',  max($modfile));
-
-        if ($dcon) {
-            // Recent database update
-            if ($stmt = $mysqli->prepare("SELECT MAX(GREATEST(updated, created)) AS date FROM `" . table . "`")) {
-                $stmt->execute();
-                $stmt->bind_result($date);
-
-                while ($stmt->fetch()) {
-                    $update = max($update, $date);
-                }
-
-                $stmt->free_result();
-                $stmt->close();
-            }
-        }
-
-        //$update = date('Y/m/d', strtotime($update));
-        return $update;
+    // Latest update
+    public static function all() {
+        self::http(self::$dbcon ? max(date::db(), date::file('appcache|css|js|php|txt|xml')) : date::file('appcache|css|js|php|txt|xml'));
     }
 }
-datemod::cache();
+
+class date {
+    private static $mysqli;
+
+    //public function __construct($mysqli) {} // MySQLi in class http://www.weblimner.com/tutorial/using-mysqli-in-a-seperate-class/
+    public static function url() {
+        global $url;
+
+        $stmt = self::$mysqli->prepare('SELECT GREATEST(updated, created) AS date FROM `' . table . '` WHERE url=? LIMIT 1');
+
+        $stmt->bind_param('s', $url);
+        $stmt->execute();
+        $stmt->bind_result($date);
+        $stmt->fetch();
+        $stmt->free_result();
+        $stmt->close();
+
+        return $date;
+    }
+
+    // Latest database update
+    public static function db() {
+        $stmt = self::$mysqli->prepare('SELECT MAX(GREATEST(updated, created)) AS date FROM `' . table . '`');
+
+        $stmt->execute();
+        $stmt->bind_result($date);
+        $stmt->fetch();
+        $stmt->free_result();
+        $stmt->close();
+
+        return $date;
+    }
+
+    // The file
+    public static function me() {
+        return date('Y-m-d H:i:s', filemtime($_SERVER['SCRIPT_FILENAME']));
+    }
+
+    // Recent file update filtered by type
+    public static function file($type) {
+        $directory = new RecursiveDirectoryIterator('.');
+        $iterator  = new RecursiveIteratorIterator($directory);
+        $regex     = new RegexIterator($iterator, '/.+\.(' . $type . ')/');
+        $date      = [];
+
+        foreach($regex as $key) $date[] = $key->getMTime();
+
+        return date('Y-m-d H:i:s', max($date));
+    }
+
+    // Latest update
+    public static function all() {
+        return max(self::db(), self::file('appcache|css|js|php|txt|xml'));
+    }
+}
