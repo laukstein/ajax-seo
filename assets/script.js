940af8edee3f5a8117576e158e0f7e4f968e14e7
Binyamin <7raivis@inbox.lv>
2017-11-27 23:51:09 +0200

    Use ESLint configuration from .eslintrc.json

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,3 @@
-/*eslint
-comma-spacing: 2,
-dot-notation: [2, {"allowKeywords": true}],
-eqeqeq: 2,
-indent: ["error", 4, { "SwitchCase": 1 }],
-key-spacing: [2, {"beforeColon": false, "afterColon": true}],
-no-console: 0,
-no-empty-function: 2,
-no-empty: ["error", { "allowEmptyCatch": true }],
-no-eval: 2,
-no-extend-native: 0,
-no-inner-declarations: 2,
-no-loop-func: 2,
-no-mixed-spaces-and-tabs: 2,
-no-multi-spaces: 2,
-no-new-func: 0,
-no-new: 0,
-no-shadow: 2,
-no-trailing-spaces: "error",
-no-undef: 0,
-no-underscore-dangle: 0,
-no-unused-vars: 0,
-no-use-before-define: 2,
-quotes: [2, "double"],
-semi: 2,
-space-before-blocks: 2,
-space-before-function-paren: [2, {"anonymous": "always", "named": "never"}],
-strict: [2, "function"]*/
-
 // Detect DOM change https://developers.google.com/web/updates/2012/02/Detect-DOM-changes-with-Mutation-Observers
 
 (function (factory) {
